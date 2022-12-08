b1da3a01770589c83e871eede4cc34d3e40a2dd4
sitiabdulaziz <sitiabdulaziz462@gmail.com>
2022-12-08 23:20:31 +0300

    This is hello world project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - this is first programm.
+ * Return: (0)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - this programm use printf
+ * Return: 0 if it succesful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
