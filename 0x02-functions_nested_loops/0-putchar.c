645c5749bc01b79d84a24173a857304d8777ed1c
sitiabdulaziz <sitiabdulaziz462@gmail.com>
2022-12-14 15:23:50 +0300

    updated task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 int main(void)
 {
 	_putchar('H');
-	_putchar('s');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('m');
 	_putchar('\n');
 
 	return (0);
